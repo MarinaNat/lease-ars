562dd25af1c415b70b6d8a4d2998c43368bdae3f
Marina Natalina <Kiska21mk@ya.ru>
2023-03-15 19:05:03 +0300

    choose

--- js/script.js
+++ js/script.js
@@ -12,31 +12,27 @@ const swiper = new Swiper(".swiper", {
 	},
 });
 
-// слайдер
+const tabItem = document.querySelectorAll('.tabs__btn-item')
+const tabContent = document.querySelectorAll('.tabs__content-item')
 
-// const tabItem = document.querySelectorAll('.tabs__btn-item');
-// const tabContent = document.querySelectorAll('.tabs__content-item');
+tabItem.forEach(function (element) {
+	element.addEventListener('click', open)
+})
 
-// tabItem.forEach(function (element) {
-// 	element.addEventListener('click', open);
-// })
+function open(evt) {
+	const tabTarger = evt.currentTarget;
+	const button = tabTarger.dataset.button;
 
-// function open(evt) {
-// 	const tabTarget = evt.currentTarget;
-// 	const button = tabTarget.dataset.button;
+	tabItem.forEach(function (item) {
+		item.classList.remove('tabs__btn-item_active')
+	})
+	tabTarger.classList.add('tabs__btn-item_active')
+	tabContent.forEach(function (item) {
+		item.classList.remove('tabs__content-item_active');
+	})
 
-// 	tabItem.forEach(function (item) {
-// 		item.classList.remove('tabs__btn-item_active')
-// 	})
-
-// 	tabContent.forEach(function (item) {
-// 		item.classList.remove('tabs__content-item_active')
-// 	})
-
-// 	tabTarget.classList.add('tabs__btn-item_active')
-// 	document.querySelector(`#${button}`).classList.add('tabs__content-item_active')
-// }
-
+	document.querySelector(`#${button}`).classList.add('tabs__content-item_active')
+}
 // бургер меню
 
 // const menuBtn = document.querySelector('.menu-btn');
